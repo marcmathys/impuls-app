cd3e71225d28502fcada1da2194c29fd173497bc
marc mathys <mmathys@gmail.com>
2020-06-16 16:58:45 +0200

    fixed services

--- lib/bt/widgets.dart
+++ lib/bt/widgets.dart
@@ -9,8 +9,12 @@ import 'package:flutter_blue/flutter_blue.dart';
 import 'package:implulsnew/styles/button.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:implulsnew/bt/Bluetooth_screen.dart';
-import 'package:flutter_isolate/flutter_isolate.dart';
+
+import 'dart:async';
+import 'dart:isolate';
 import 'package:isolate_handler/isolate_handler.dart';
+import 'dart:ui';
+import 'package:uuid/uuid.dart';
 
 class ScanResultTile extends StatelessWidget {
   const ScanResultTile({Key key, this.result, this.onTap}) : super(key: key);
@@ -181,7 +185,6 @@ class CharacteristicTile extends StatefulWidget {
   _CharacteristicTileState createState() => _CharacteristicTileState();
 }
 
-
 //StreamBuilder<List<int>>(
 //stream: widget.characteristic.value,
 //initialData: widget.characteristic.lastValue,
@@ -190,27 +193,65 @@ class CharacteristicTile extends StatefulWidget {
 //
 //print(_btData);
 
-
-
-
 class _CharacteristicTileState extends State<CharacteristicTile> {
   final List<MedicalData> _chartData = [
     MedicalData(DateTime.now(), 0),
   ];
   final _listData = [];
 
+//  final isolates = IsolateHandler();
+//  int counter = 0;
+//
+//  void main() {
+//    // Start the isolate at the `entryPoint` function.
+//    isolates.spawn<int>(entryPoint,
+//        name: "counter",
+//        // Executed every time data is received from the spawned isolate.
+//        onReceive: setCounter,
+//        // Executed once when spawned isolate is ready for communication.
+//        onInitialized: () => isolates.send(counter, to: "counter")
+//    );
+//  }
+//
+//// Set new count and display current count.
+//  void setCounter(int count) {
+//    counter = count;
+//    print("Counter is now $counter");
+//
+//    // We will no longer be needing the isolate, let's dispose of it.
+//    isolates.kill("counter");
+//  }
+//
+//// This function happens in the isolate.
+//  void entryPoint(Map<String, dynamic> context) {
+//    // Calling initialize from the entry point with the context is
+//    // required if communication is desired. It returns a messenger which
+//    // allows listening and sending information to the main isolate.
+//    final messenger = HandledIsolate.initialize(context);
+//
+//    // Triggered every time data is received from the main isolate.
+//    messenger.listen((count) {
+//      // Add one to the count and send the new value back to the main
+//      // isolate.
+//      messenger.send(++count);
+//    });
+//  }
+
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<List<int>>(
-      stream: widget.characteristic.value,
-      initialData: widget.characteristic.lastValue,
-      builder: (c, snapshot) {
-        var _btData = snapshot.data;
+    return Center(
 
-        byteConversion(_btData);
+      child: StreamBuilder<List<int>>(
+        stream: widget.characteristic.value,
+        initialData: widget.characteristic.lastValue,
+        builder: (c, snapshot) {
+          var _btData = snapshot.data;
 
-        return buildColumn(context, snapshot);
-      },
+          byteConversion(_btData);
+
+          return buildColumn(context, snapshot);
+        },
+      ),
     );
   }
 
@@ -220,16 +261,16 @@ class _CharacteristicTileState extends State<CharacteristicTile> {
       if (_chartData.length > 300) {
         _chartData.removeAt(0);
       }
-      ByteData bytedata1 = ByteData.sublistView(
-          Uint8List.fromList(_btData.reversed.toList()));
+      ByteData bytedata1 =
+          ByteData.sublistView(Uint8List.fromList(_btData.reversed.toList()));
       print(bytedata1);
       int _ekgPoint = bytedata1.getInt16(0, Endian.big);
       print(_ekgPoint);
-    
+
       _chartData.add(MedicalData(DateTime.now(), _ekgPoint));
     } else if (_btData.length == 4) {
-      ByteData bytedata2 = ByteData.sublistView(
-          Uint8List.fromList(_btData.reversed.toList()));
+      ByteData bytedata2 =
+          ByteData.sublistView(Uint8List.fromList(_btData.reversed.toList()));
       print(bytedata2);
       if (widget.characteristic.serviceUuid.toString() ==
           '00b3b02e-928b-11e9-bc42-526af7764f64') {
@@ -247,85 +288,81 @@ class _CharacteristicTileState extends State<CharacteristicTile> {
 
   Column buildColumn(BuildContext context, AsyncSnapshot<List<int>> snapshot) {
     return Column(
-        children: <Widget>[
-          ExpansionTile(
-            title: ListTile(
-              title: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(widget.characteristic.uuid
-                          .toString()
-                          .contains('526af7764f64')
-                      ? 'EKG'
-                      : ''),
-                  Text(widget.characteristic.uuid
-                          .toString()
-                          .contains('df60bd72')
-                      ? 'IBI'
-                      : ''),
-                  Text('${widget.characteristic.uuid.toString()}',
-                      style: Theme.of(context).textTheme.bodyText2.copyWith(
-                          color: Theme.of(context).textTheme.caption.color))
-                ],
-              ),
-              subtitle: Text(snapshot.data.toString()),
-              contentPadding: EdgeInsets.all(0.0),
-            ),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        ExpansionTile(
+          title: ListTile(
+            title: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                SizedBox(
-                  width: 40,
-                ),
-                IconButton(
-                  icon: Icon(Icons.edit,
-                      color:
-                          Theme.of(context).iconTheme.color.withOpacity(0.5)),
-                  onPressed: widget.onWritePressed,
-                ),
-                Text('W'),
-                SizedBox(
-                  width: 40,
-                ),
-                IconButton(
-                  icon: Icon(
-                      widget.characteristic.isNotifying
-                          ? Icons.sync_disabled
-                          : Icons.sync,
-                      color:
-                          Theme.of(context).iconTheme.color.withOpacity(0.5)),
-                  onPressed: widget.onNotificationPressed,
-                ),
-                ButtonButton(
-                  onPressed: () async {
-                    Text('pressed');
-                    Text('$writeToDeviceBytes()');
-                  },
-                  child: Container(
-                    width: 300,
-                    color: Colors.indigo.shade50,
-                    child: TextField(
-                      onChanged: (text) {
-                        writeInput = text;
-                      },
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: Colors.black),
-                      decoration: InputDecoration(
-                          labelText: "Choose Service, then W",
-                          border: OutlineInputBorder()),
-                    ),
-                  ),
-                ),
+                Text(widget.characteristic.uuid
+                        .toString()
+                        .contains('526af7764f64')
+                    ? 'EKG'
+                    : ''),
+                Text(widget.characteristic.uuid.toString().contains('df60bd72')
+                    ? 'IBI'
+                    : ''),
+                Text('${widget.characteristic.uuid.toString()}',
+                    style: Theme.of(context).textTheme.bodyText2.copyWith(
+                        color: Theme.of(context).textTheme.caption.color))
               ],
             ),
-            children: widget.descriptorTiles,
+            subtitle: Text(snapshot.data.toString()),
+            contentPadding: EdgeInsets.all(0.0),
           ),
-          (snapshot.data.length == 2)
-              ? Chart(chartData: _chartData)
-              : ScrollList(listData: _listData),
-        ],
-      );
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              SizedBox(
+                width: 40,
+              ),
+              IconButton(
+                icon: Icon(Icons.edit,
+                    color: Theme.of(context).iconTheme.color.withOpacity(0.5)),
+                onPressed: widget.onWritePressed,
+              ),
+              Text('W'),
+              SizedBox(
+                width: 40,
+              ),
+              IconButton(
+                icon: Icon(
+                    widget.characteristic.isNotifying
+                        ? Icons.sync_disabled
+                        : Icons.sync,
+                    color: Theme.of(context).iconTheme.color.withOpacity(0.5)),
+                onPressed: widget.onNotificationPressed,
+              ),
+              ButtonButton(
+                onPressed: () async {
+                  Text('pressed');
+                  Text('$writeToDeviceBytes()');
+                },
+                child: Container(
+                  width: 300,
+                  color: Colors.indigo.shade50,
+                  child: TextField(
+                    onChanged: (text) {
+                      writeInput = text;
+                    },
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.black),
+                    decoration: InputDecoration(
+                        labelText: "Choose Service, then W",
+                        border: OutlineInputBorder()),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          children: widget.descriptorTiles,
+        ),
+        (snapshot.data.length == 2)
+            ? Chart(chartData: _chartData)
+            : ScrollList(listData: _listData),
+      ],
+    );
   }
 }
 
@@ -392,8 +429,7 @@ class ScrollList extends StatelessWidget {
           reverse: true,
           itemBuilder: (BuildContext ctxt, int index) {
             return Container(
-                height: 30,
-                child: Text((_listData != [] ? '$_listData' : '')));
+                height: 30, child: Text((_listData != [] ? '$_listData' : '')));
           }),
     );
   }
@@ -405,10 +441,7 @@ class DescriptorTile extends StatelessWidget {
   final VoidCallback onWritePressed;
 
   const DescriptorTile(
-      {Key key,
-      this.descriptor,
-        this.onReadPressed,
-      this.onWritePressed})
+      {Key key, this.descriptor, this.onReadPressed, this.onWritePressed})
       : super(key: key);
 
   @override
@@ -441,7 +474,6 @@ class DescriptorTile extends StatelessWidget {
             ),
             onPressed: onReadPressed,
           ),
-
           IconButton(
             icon: Icon(
               Icons.file_upload,
